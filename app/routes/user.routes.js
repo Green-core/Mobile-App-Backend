41b28bc55a6cf9213b72379ecb4fd6119973e7a6
codekcg23 <kavigamage23@gmail.com>
2020-06-07 13:24:19 +0530

    2020007-01-login and register

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/users.model");
+const bcrypt =require("bcrypt");
+const verifyToken = require('../middleware/authJWT');
+const jwt=require("jsonwebtoken");
+const config=require('../../config/auth.config.js');
 
 /**
  * @route   POST /users/save
@@ -79,5 +83,102 @@ router.put("/update/:id", (req, res) => {
     });
   res.status(200);
 });
+
+
+/**
+ * @route   Post /user/register/
+ * @desc    Register user
+ * @access  Private
+ */
+
+router.post('/register',(req,res)=>{
+
+  User
+    .findOne({email:req.body.email})
+    .then(user =>{
+      if(user){
+        return res.status(400).json({ email: "Email already exists" });
+      }
+      else{
+      
+          bcrypt.hash(req.body.password, 10, (err, hash) => {
+            if (err) throw err;
+            const newUser = new User({
+              name: req.body.name,
+              email: req.body.email,
+              password: hash
+              // TODO : isEmailVerified: false   email verfication
+           })
+           // newUser.password = hash;
+            newUser
+              .save()
+              .then(user => res.json(user))
+              .catch(err => console.log(err));
+          });
+      
+      }
+    })
+    .catch(err=>{
+      console.log(err);
+      return res.status(400).json(err);
+    })
+
+})
+
+
+ /**
+ * @route   Post /user/login/
+ * @desc    login user
+ * @access  Private
+ */
+
+router.post('/login',(req,res)=>{
+
+  const email =req.body.email;
+  const password =req.body.password;
+
+  User
+    .findOne({email})
+    .then(user =>{
+      if(user){
+        bcrypt.compare(password,user.password).then(isMatch=>{
+          if(isMatch){
+              // User matched
+              // Create JWT Payload 
+              const payload={
+                  id: user._id,
+                  name:user.name,
+                  email:user.email
+              };
+              //sign token
+              jwt.sign(
+                  payload,
+                  config.secret,
+                  {
+                      expiresIn: 86400//1 day in seconds
+                  },
+                  (err,token)=>{
+                      res.json({
+                          success:true,
+                          message: 'Authentication successful!',
+                          token:"Bearer "+token
+                      });
+                  }
+              );
+          }else{
+              return res.status(400).json({incorrect: "Incorrect Username or password"});
+          }
+      });
+
+      }
+      else{
+        return res.status(404).json({incorrect: "Incorrect Username or password"});
+      }
+    })
+    .catch(err=>{
+      console.log(err);
+      return res.status(400).json(err);
+    })
+})
 
 module.exports = router;
