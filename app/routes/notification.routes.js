7bdb198a86f8245fe52ed1c542ebdbf3b2af6fe7
Nipuna <chandimal.nipuna.1995@gmail.com>
2020-07-17 14:49:02 +0530

    changed the structure

--- app/routes/notification.routes.js
+++ app/routes/notification.routes.js
@@ -15,68 +15,68 @@ router.get("/check/:id", (req, res) => {
 
   Unit.find({ userID: req.params.id })
     .then((docs) => {
-      
       for (var a in docs) {
-
         //check sensors one by one
         var unitData = docs[a];
         var unitAlert = false;
         var unitAlerts = null;
-        var soilMoistureSensorAlert = null; 
-        var lightIntensitySensorAlert = null; 
+        var soilMoistureSensorAlert = null;
+        var lightIntensitySensorAlert = null;
 
+        var alertSoilMoisture = false;
+        var alertLightIntensity = false;
 
-        var alertSoilMoisture = false;  
-        var alertLightIntensity = false;  
-
- 
-        if (parseInt(unitData.soilMoistureSensor.lastReading) < 20 ) {
-          alertSoilMoisture = true; 
+        if (parseInt(unitData.soilMoistureSensor.lastReading) < 20) {
+          alertSoilMoisture = true;
           unitAlert = true;
-          soilMoistureSensorAlert = { 
-              value: unitData.soilMoistureSensor.lastReading,
-              time: new Date(), 
-              name: unitData.unitName, 
+          soilMoistureSensorAlert = {
+            value: unitData.soilMoistureSensor.lastReading,
+            time: new Date(),
+            name: unitData.unitName,
+            type: "SM",
           };
-        } 
+        }
 
-        if ( parseInt(unitData.lightIntensitySensor.lastReading.substring( 0, unitData.lightIntensitySensor.lastReading.length - 3 )) < 20 ) {
-          alertLightIntensity = true; 
+        if (
+          parseInt(
+            unitData.lightIntensitySensor.lastReading.substring(
+              0,
+              unitData.lightIntensitySensor.lastReading.length - 3
+            )
+          ) < 20
+        ) {
+          alertLightIntensity = true;
           unitAlert = true;
-          console.log(unitData['unitName'])
-          lightIntensitySensorAlert = { 
-              value: unitData.lightIntensitySensor.lastReading.substring(0,unitData.lightIntensitySensor.lastReading.length - 3),
-              time: new Date(), 
-              name: unitData.unitName, 
+          console.log(unitData["unitName"]);
+          lightIntensitySensorAlert = {
+            value: unitData.lightIntensitySensor.lastReading.substring(
+              0,
+              unitData.lightIntensitySensor.lastReading.length - 3
+            ),
+            time: new Date(),
+            name: unitData.unitName,
+            type: "LT",
           };
-        } 
-        var alertObject = {} 
+        }
 
-        if(alertSoilMoisture){
-          alertObject = {
-             SM:soilMoistureSensorAlert
-          }
+        if (alertSoilMoisture) {
+          alerts.push({
+            [unitData["moduleID"] + "SM"]: soilMoistureSensorAlert,
+          });
         }
-       
-        if( alertLightIntensity){
-          alertObject = {
-            ...alertObject ,
-             LT:lightIntensitySensorAlert 
-          }
+
+        if (alertLightIntensity) {
+          alerts.push({
+            [unitData["moduleID"] + "LT"]: lightIntensitySensorAlert,
+          });
         }
-      
-        unitAlerts = { ...alertObject   }
-       
-        if(unitAlert){ 
-          alerts.push(unitAlerts)
-        } 
+
         unitAlert = false;
       }
-      console.log(JSON.stringify({state:alerts.length !=0,data:alerts}, null, 2)); 
-      res.send( 
-        {state:alerts.length !=0 ,
-        data:alerts}
-        ); 
+      console.log(
+        JSON.stringify({ state: alerts.length != 0, data: alerts }, null, 2)
+      );
+      res.send({ state: alerts.length != 0, data: alerts });
     })
     .catch((err) => {
       console.log(err);
